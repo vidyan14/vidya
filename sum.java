5ec75ca3c8fa264927e69950a8673991b2de5897
vidya <anusha2.gs@gmail.com>
2023-09-07 16:19:17 +0530

    added message in sum.java

--- sum.java
+++ sum.java
@@ -4,6 +4,7 @@ public static void main(String args[])
 {  
 int n1 = 225, n2 = 115, sum;  
 sum = n1 + n2;  
-System.out.println("The sum of numbers is: "+sum);  
+System.out.println("The sum of numbers is: "+sum); 
+system.out.println("welcome to cse") 
 }  
 }  
